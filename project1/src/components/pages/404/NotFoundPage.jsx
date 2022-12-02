42d038762030ca5bf34cbece33d44e2da6cc4057
mariantirlea <mariantirlea@hotmail.com>
2022-12-02 22:32:53 +0200

    Align 404 page content to middle

--- project1/src/components/pages/404/NotFoundPage.jsx
+++ project1/src/components/pages/404/NotFoundPage.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 function NotFoundPage(){
     return (
-        <div className="text-center">
+        <div style={{minHeight: '100%'}} className="pb-3 text-center flex-column d-flex justify-content-center align-items-center">
             <h1 className="display-1 fw-bold">404</h1>
             <p className="fs-3"> <span className="text-danger">Ups!</span> Pagină negasită.</p>
             <p className="lead">Pagina pe care o căutați nu există.</p>
