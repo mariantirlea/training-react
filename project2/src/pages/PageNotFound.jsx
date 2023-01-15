3d85e542561c42b7f8f052002c344cf68446d7fb
Marian Tîrlea <mariantirlea@hotmail.com>
2023-01-15 03:22:40 +0200

    Fix page not found

--- project2/src/pages/PageNotFound.jsx
+++ project2/src/pages/PageNotFound.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function PageNotFound(){
-    return <h1>404</h1>
+    return <div style={{minHeight: '100%', marginTop: '10%'}} className="text-white pb-3 text-center flex-column d-flex justify-content-center align-items-center">
+    <h1 className="display-1 fw-bold">404</h1>
+    <p className="fs-3"> <span className="text-danger">Ups!</span> Pagină negasită.</p>
+    <p className="lead">Pagina pe care o căutați nu există.</p>
+
+    <Link
+        to="/"
+        className="btn btn-light">
+        Pagina principală
+    </Link>
+
+</div>
 }
 
 export default PageNotFound;
